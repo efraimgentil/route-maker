27d633774fd118665d7f0179a5d1dcb7ecb17294
root <efraimgentil@gmail.com>
2016-07-19 21:12:06 -0300

    Removing useless directive

--- route-maker/app/directive/StaticInclude.js
+++
@@ -1,6 +0,0 @@
-angular.module( moduleName ).directive( "staticInclude", [  function(){
-  return {
-      restrict: 'AE'
-      ,templateUrl: function(elem, attrs) { return attrs.ngIncludeSrc; }
-  };
-}]);
