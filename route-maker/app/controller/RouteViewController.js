c4d3b606573b87bf4ff631d1e6f47b1b8e0a3397
root <efraimgentil@gmail.com>
2016-08-28 12:45:22 -0300

    WOrking on the route printing

--- route-maker/app/controller/RouteViewController.js
+++ route-maker/app/controller/RouteViewController.js
@@ -17,5 +17,46 @@ angular.module(moduleName).controller("RouteViewController", [
     $scope.cancel = function(){
       $location.path("/route");
     };
+
+    $scope.print = function(){
+      var body               = $('body');
+      var mapContainer       = $('.map-container');
+      var mapContainerParent = mapContainer.parent();
+      var printContainer     = $('<div>');
+      var fields = $('.fields');
+      var form = $('#formViewRoute');
+
+      printContainer
+        .addClass('print-container')
+        .css('position', 'relative')
+        .append( fields )
+        .height(mapContainer.height())
+        .append(mapContainer)
+        .prependTo(body);
+
+      var content = body
+        .children()
+        .not('script')
+        .not(printContainer)
+        .detach();
+
+      // Patch for some Bootstrap 3.3.x `@media print` styles. :|
+      var patchedStyle = $('<style>')
+        .attr('media', 'print')
+        .text('img { max-width: none !important; }' +
+          'a[href]:after { content: ""; }')
+        .appendTo('head');
+
+      window.print();
+
+      body.prepend(content);
+      form.prepend( fields );
+      mapContainerParent.prepend(mapContainer);
+
+      printContainer.remove();
+      patchedStyle.remove();
+
+    };
+
   }
 ]);
